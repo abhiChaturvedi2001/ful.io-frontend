944b9fb9d6eb7ca1df61911ca2677ded53f50082
ABHCH <chaturvedia435@gmail.com>
2025-01-18 00:10:12 +0530

    adding

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,2 @@
-export const base_api_url = `http://localhost:4000/auth`
+// export const base_api_url = `http://localhost:4000/auth`
+export const base_api_url = `https://fuliobackend-fbq8zsivc-abhchs-projects.vercel.app`
